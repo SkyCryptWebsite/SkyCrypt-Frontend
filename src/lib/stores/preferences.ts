20d07f2f179461112b72d6cf9d97704b2bd32723
DarthGigi <47110839+DarthGigi@users.noreply.github.com>
2025-07-01 21:49:05 +0200

    refactor: remove internalPreferences store to simplify preferences management

--- src/lib/stores/preferences.ts
+++ src/lib/stores/preferences.ts
@@ -2,12 +2,6 @@ import { sections } from "$lib/sections/constants";
 import type { SectionID } from "$lib/sections/types";
 import { persisted } from "svelte-persisted-store";
 
-// First param `internalPreferences` is the local storage key.
-// Second param is the initial value.
-export const internalPreferences = persisted("internalPreferences", {
-  hasSeenv2Toast: false
-});
-
 export const sectionOrderPreferences = persisted<SectionID[]>("sectionOrderPreferences", sections);
 
 export const performanceMode = persisted<boolean>("performanceMode", false);
