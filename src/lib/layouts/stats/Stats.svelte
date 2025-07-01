413761382e530205ccc6967c11d582f5529c1588
DarthGigi <47110839+DarthGigi@users.noreply.github.com>
2025-07-01 21:15:33 +0200

    refactor: enhance Collapsible component handling and improve loading state display

--- src/lib/layouts/stats/Stats.svelte
+++ src/lib/layouts/stats/Stats.svelte
@@ -10,6 +10,7 @@
   import { quadInOut } from "svelte/easing";
   import { slide } from "svelte/transition";
 
+  let openState = $state(false);
   const ctx = getProfileCtx();
   const profile = $derived(ctx.profile);
   const profileUUID = $derived(profile.uuid);
@@ -29,6 +30,7 @@
 
 <div class="stats flex flex-col">
   <Collapsible.Root
+    bind:open={openState}
     onOpenChange={(open) => {
       if (open && !$query.isFetched) {
         $query.refetch();
@@ -37,9 +39,6 @@
     <Collapsible.Content forceMount class="columns-[12.5rem]">
       {#snippet child({ props, open })}
         {#if open}
-          {#if $query.isPending}
-            <LoaderCircle class="text-icon animate-spin" />
-          {/if}
           {#if $query.error}
             <Error />
           {/if}
@@ -55,6 +54,12 @@
         {/if}
       {/snippet}
     </Collapsible.Content>
-    <Collapsible.Trigger class="bg-text/10 mx-auto mt-3.5 w-full rounded-full p-2.5 text-xs font-semibold uppercase">Show Stats</Collapsible.Trigger>
+    <Collapsible.Trigger class="bg-text/10 mx-auto mt-3.5 w-full rounded-full p-2.5 text-xs font-semibold uppercase">
+      {#if $query.isLoading}
+        <LoaderCircle class="text-icon mx-auto animate-spin" />
+      {:else}
+        {openState ? "Hide Stats" : "Show Stats"}
+      {/if}
+    </Collapsible.Trigger>
   </Collapsible.Root>
 </div>
