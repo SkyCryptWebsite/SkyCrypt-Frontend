7face35eb3d22f034691dde1a7f6f9b32412ed06
DarthGigi <47110839+DarthGigi@users.noreply.github.com>
2025-07-06 00:55:37 +0200

    feat: add informational notice for missing foraging data due to API limitations

--- src/lib/sections/stats/SkillsSection.svelte
+++ src/lib/sections/stats/SkillsSection.svelte
@@ -40,6 +40,10 @@
     <Notice title="An unexpected error has occurred" type="error" />
   {/if}
   {#if $query.isSuccess && $query.data && skills}
+    <Notice type="info" title="Foraging" class="my-5">
+      <p class="text-text/80">Unfortunately, Hypixel has yet to add the new foraging update to their API.<br />Until they do, we can't show any foraging related data, as it simply doesn't exist.</p>
+      <p class="text-text/80">We will add foraging as soon as Hypixel adds it to their API.</p>
+    </Notice>
     {#if skills.mining}
       <Mining />
     {:else}
