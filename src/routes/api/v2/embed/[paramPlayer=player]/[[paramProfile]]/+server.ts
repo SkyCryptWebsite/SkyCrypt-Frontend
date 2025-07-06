5e35508cdce08a6420e9b27374dabe4f6bf7e658
DuckySoLucky <robertkovac160@gmail.com>
2025-07-06 11:26:50 +0200

    fix: accessory enrichment, and embed endpoint

--- src/routes/api/v2/embed/[paramPlayer=player]/[[paramProfile]]/+server.ts
+++ src/routes/api/v2/embed/[paramPlayer=player]/[[paramProfile]]/+server.ts
@@ -1,12 +1,13 @@
 import { REDIS } from "$lib/server/db/redis.js";
 import { getProfile, getUUID } from "$lib/server/lib.js";
+import { isUUID } from "$params/uuid.js";
 import { json } from "@sveltejs/kit";
 
 export async function GET({ params }) {
   const { paramPlayer, paramProfile = null } = params;
 
   const uuid = await getUUID(paramPlayer, { cache: true });
-  const profileId = !paramProfile ? (await getProfile(uuid, null, { cache: true })).profile_id : paramProfile;
+  const profileId = !paramProfile || !isUUID(paramProfile) ? (await getProfile(uuid, paramProfile, { cache: true })).profile_id : paramProfile;
 
   const data = await REDIS.get(`embed_data:${uuid}:${profileId}`);
   if (!data) {
