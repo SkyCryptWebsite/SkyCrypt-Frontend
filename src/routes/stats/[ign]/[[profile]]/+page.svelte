549dee9aaf995882c408dc679959198660f64252
DarthGigi <47110839+DarthGigi@users.noreply.github.com>
2025-07-28 23:27:51 +0200

    fix: comment out afterNavigate function to prevent unnecessary page reloads

--- src/routes/stats/[ign]/[[profile]]/+page.svelte
+++ src/routes/stats/[ign]/[[profile]]/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import { afterNavigate } from "$app/navigation";
   import { page } from "$app/state";
   import Notice from "$lib/components/Notice.svelte";
   import Main from "$lib/layouts/stats/Main.svelte";
@@ -53,17 +52,17 @@
     }
   });
 
-  afterNavigate(async ({ from, to, willUnload }) => {
-    if (!from || !to) return;
-    const { params: fromParams } = from;
-    const { params: toParams } = to;
-    if (!fromParams || !toParams) return;
-    if ((fromParams.ign !== toParams.ign || fromParams.profile !== toParams.profile) && !willUnload) {
-      console.warn("IGN or Profile changed, reloading page to reflect new profile.");
-      // Hard reload the page if the IGN changes, this ensures the profile context is updated correctly as TanStack Query does not work with Svelte 5 runes/states yet.
-      window.location.reload();
-    }
-  });
+  // afterNavigate(async ({ from, to, willUnload }) => {
+  //   if (!from || !to) return;
+  //   const { params: fromParams } = from;
+  //   const { params: toParams } = to;
+  //   if (!fromParams || !toParams) return;
+  //   if ((fromParams.ign !== toParams.ign || fromParams.profile !== toParams.profile) && !willUnload) {
+  //     console.warn("IGN or Profile changed, reloading page to reflect new profile.");
+  //     // Hard reload the page if the IGN changes, this ensures the profile context is updated correctly as TanStack Query does not work with Svelte 5 runes/states yet.
+  //     window.location.reload();
+  //   }
+  // });
 </script>
 
 {#await data.stats}
