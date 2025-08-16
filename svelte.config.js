bee27fb5258d598690561a38fd34891f4f317ed2
DarthGigi <47110839+DarthGigi@users.noreply.github.com>
2025-08-16 18:56:17 +0200

    fix: remove frame-ancestors directive from CSP configuration to allow embedding

--- svelte.config.js
+++ svelte.config.js
@@ -46,8 +46,7 @@ const config = {
         "style-src": ["self", "unsafe-inline", "https://fonts.googleapis.com"],
         "img-src": ["self", "data:", "https://vzge.me", "https://crafatar.com", "https://mc-heads.net"],
         "connect-src": ["self", "https://crafatar.com"],
-        "font-src": ["self", "https://fonts.gstatic.com"],
-        "frame-ancestors": ["*"]
+        "font-src": ["self", "https://fonts.gstatic.com"]
       }
     }
   },
