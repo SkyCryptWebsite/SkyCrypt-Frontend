9fdc47d56f93651c57b68013f85713e54af64c76
DarthGigi <47110839+DarthGigi@users.noreply.github.com>
2025-10-05 18:40:17 +0200

    feat: enhance Settings component with tooltips and additional descriptions

--- src/lib/components/header/Settings.svelte
+++ src/lib/components/header/Settings.svelte
@@ -13,6 +13,7 @@
   import { wikiOrderPreferences } from "$lib/stores/wiki";
   import BookOpenText from "@lucide/svelte/icons/book-open-text";
   import Check from "@lucide/svelte/icons/check";
+  import CircleQuestionMark from "@lucide/svelte/icons/circle-question-mark";
   import Cog from "@lucide/svelte/icons/cog";
   import Fan from "@lucide/svelte/icons/fan";
   import GripVertical from "@lucide/svelte/icons/grip-vertical";
@@ -24,7 +25,7 @@
   import Settings from "@lucide/svelte/icons/settings";
   import Settings2 from "@lucide/svelte/icons/settings-2";
   import Sparkle from "@lucide/svelte/icons/sparkle";
-  import { Avatar, Button, Label, Popover, RadioGroup, Separator, Switch, Tabs } from "bits-ui";
+  import { Avatar, Button, Label, Popover, RadioGroup, Separator, Switch, Tabs, Tooltip } from "bits-ui";
   import { getContext, onMount } from "svelte";
   import { dndzone, SHADOW_ITEM_MARKER_PROPERTY_NAME } from "svelte-dnd-action";
   import { flip } from "svelte/animate";
@@ -227,15 +228,47 @@
     <Tabs.Content value={SettingsTab.Misc} class="space-y-6">
       <div class="flex max-h-96 flex-col gap-4 overflow-x-clip overflow-y-auto">
         <div class="bg-text/[0.05] space-y-4 rounded-lg p-4">
-          <h4 class="flex items-center gap-2 rounded-lg p-2 font-semibold">
-            <Settings2 class="size-5" />
-            Misc Settings
-          </h4>
+          <div class="flex items-start gap-2 rounded-lg p-2 font-semibold">
+            <Settings2 class="size-5 h-lh shrink-0" />
+            <div>
+              <h4>Misc Settings</h4>
+              <div class="space-y-2">
+                <p class="text-text/60">These settings affect various parts of SkyCrypt.</p>
+                <p class="text-text/60">They are saved in your browser and will persist across sessions.</p>
+              </div>
+            </div>
+          </div>
           <Label.Root for="performance" class="bg-text/[0.05] flex items-center justify-between gap-4 rounded-lg p-2">
-            <div class="flex items-center gap-2">
-              <Fan class="data-[performance=false]:animate-spin-slow size-6 will-change-transform data-[performance=true]:animate-spin" data-performance={$performanceMode} />
+            <div class="flex items-start gap-2">
+              <Fan class="data-[performance=false]:animate-spin-slow size-6 h-lh shrink-0 will-change-transform data-[performance=true]:animate-spin" data-performance={$performanceMode} />
               <div class="flex flex-col">
-                <h4 class="text-text/90 font-semibold">Performance Mode</h4>
+                <Tooltip.Provider delayDuration={0}>
+                  <Tooltip.Root>
+                    <Tooltip.Trigger class="flex items-center gap-1">
+                      <h4 class="text-text/90 font-semibold">Performance Mode</h4>
+                      <CircleQuestionMark class="text-text/60 size-4 h-lh" />
+                    </Tooltip.Trigger>
+                    <Tooltip.Portal>
+                      <Tooltip.Content forceMount class={cn("z-50 flex w-full max-w-lg flex-col space-y-2 overflow-hidden rounded-lg p-4 select-text", $performanceMode ? "bg-background-grey" : "backdrop-blur-lg backdrop-brightness-50")}>
+                        {#snippet child({ wrapperProps, props, open })}
+                          {#if open}
+                            <div {...wrapperProps}>
+                              <div {...props} transition:flyAndScale>
+                                <p>You might not need this! We've noticed that often the reason for low performance is due to Graphics Acceleration being disabled in the browser settings.</p>
+                                <p>Harware Acceleration gives the browsers access to your GPU for rendering, which can significantly improve performance; especially with opacity and blur effects.</p>
+                                <p>
+                                  Enable <a href="https://www.google.com/search?q=enable+graphics+acceleration+in+%5Bbrowser%5D" target="_blank" rel="noopener noreferrer" class="text-icon underline">Graphics Acceleration</a> in your browser settings first, and if you still experience performance issues, then consider enabling Performance Mode.
+                                </p>
+                                <Tooltip.Arrow />
+                              </div>
+                            </div>
+                          {/if}
+                        {/snippet}
+                      </Tooltip.Content>
+                    </Tooltip.Portal>
+                  </Tooltip.Root>
+                </Tooltip.Provider>
+                <p class="text-text/60">Disables blur, transparency and backdrop effects for better performance on low-end devices.</p>
               </div>
             </div>
             <Switch.Root id="performance" checked={$performanceMode} class="data-[state=checked]:bg-icon data-[state=unchecked]:bg-text/30 peer inline-flex h-6 min-h-6 w-10 shrink-0 cursor-pointer items-center rounded-full px-0 transition-colors ease-out" onCheckedChange={() => performanceMode.update((value) => !value)}>
@@ -244,10 +277,11 @@
           </Label.Root>
 
           <Label.Root for="glint" class="bg-text/[0.05] flex items-center justify-between gap-4 rounded-lg p-2">
-            <div class="flex items-center gap-2">
-              <Sparkle class="size-6" />
+            <div class="flex items-start gap-2">
+              <Sparkle class="size-6 h-lh shrink-0" />
               <div class="flex flex-col">
                 <h4 class="text-text/90 font-semibold">Show Glint</h4>
+                <p class="text-text/60">Show the enchantment glint effect on enchanted items.</p>
               </div>
             </div>
             <Switch.Root id="glint" checked={$showGlint} class="data-[state=checked]:bg-icon data-[state=unchecked]:bg-text/30 peer inline-flex h-6 min-h-6 w-10 shrink-0 cursor-pointer items-center rounded-full px-0 transition-colors ease-out" onCheckedChange={() => showGlint.update((value) => !value)}>
@@ -256,10 +290,11 @@
           </Label.Root>
 
           <Label.Root for="rainbow" class="bg-text/[0.05] flex items-center justify-between gap-4 rounded-lg p-2">
-            <div class="flex items-center gap-2">
-              <Rainbow class="size-6" />
+            <div class="flex items-start gap-2">
+              <Rainbow class="size-6 h-lh shrink-0" />
               <div class="flex flex-col">
                 <h4 class="text-text/90 font-semibold">Rainbow Colors</h4>
+                <p class="text-text/60">Enable rainbow colors animation for maxed enchants on items.</p>
               </div>
             </div>
             <Switch.Root
@@ -275,10 +310,11 @@
           </Label.Root>
 
           <div class="bg-text/[0.05] flex items-center justify-between gap-4 rounded-lg p-2">
-            <div class="flex items-center gap-2">
-              <Keyboard class="size-6 " />
+            <div class="flex items-start gap-2">
+              <Keyboard class="size-6 h-lh shrink-0" />
               <div class="flex flex-col">
                 <h4 class="text-text/90 font-semibold">Keybind</h4>
+                <p class="text-text/60">Set the keybind to open the command menu</p>
               </div>
             </div>
             <Button.Root class="bg-text/10 hover:bg-text/20 border-text/20 text-text/90 focus:ring-icon/50 flex h-8 min-w-8 items-center justify-center rounded-md border px-2 py-1 font-mono text-sm font-semibold transition-colors ease-out focus:ring-2 focus:outline-none" onclick={handleKeybindClick} onkeydown={handleKeybindKeydown} tabindex={0}>
@@ -292,10 +328,16 @@
         </div>
         <Separator.Root class="bg-icon/30 shrink-0 data-[orientation=horizontal]:h-0.5 data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-0.5" />
         <div class="bg-text/[0.05] space-y-4 rounded-lg p-4">
-          <h4 class="flex items-center gap-2 rounded-lg p-2 font-semibold">
-            <BookOpenText class="size-5" />
-            Wiki Order
-          </h4>
+          <div class="flex items-start gap-2 rounded-lg p-2 font-semibold">
+            <BookOpenText class="size-5 h-lh shrink-0" />
+            <div class="">
+              <h4>Wiki Order</h4>
+              <div class="space-y-2">
+                <p class="text-text/60">Drag and drop the wiki sources to reorder them as you like.</p>
+                <p class="text-text/60">If the wiki source isn't available, the next one in the list will be used.</p>
+              </div>
+            </div>
+          </div>
           <div
             class="flex max-h-96 flex-col gap-4 overflow-x-clip overflow-y-auto"
             use:dndzone={{ items: wikiOrder, flipDurationMs: 300, dropTargetStyle: {} }}
@@ -307,12 +349,18 @@
             {#each wikiOrder as wiki (wiki.id)}
               {@const normalizedName = wiki.name.replaceAll("_", " ")}
               <div animate:flip={{ duration: 300, easing: cubicOut }} class="bg-text/[0.05] relative flex items-center gap-2 rounded-lg p-2 font-semibold">
-                <GripVertical class="text-text/60 size-5" />
-                {normalizedName}
+                <GripVertical class="text-text/60 size-5 shrink-0" />
+                <div class="flex flex-col">
+                  {normalizedName}
+                  <Button.Root href={wiki.link} target="_blank" class="text-link/60 underline">{new URL(wiki.link).hostname}</Button.Root>
+                </div>
                 {#if SHADOW_ITEM_MARKER_PROPERTY_NAME in wiki && wiki[SHADOW_ITEM_MARKER_PROPERTY_NAME]}
                   <div in:fade={{ duration: 300, easing: cubicOut }} class="bg-text/[0.05] visible absolute inset-0 flex animate-pulse items-center gap-2 rounded-lg p-2 font-semibold opacity-30">
-                    <GripVertical class="text-text/60 size-5" />
-                    {normalizedName}
+                    <GripVertical class="text-text/60 size-5 shrink-0" />
+                    <div class="flex flex-col">
+                      {normalizedName}
+                      <Button.Root href={wiki.link} target="_blank" class="text-link/60 underline">{new URL(wiki.link).hostname}</Button.Root>
+                    </div>
                   </div>
                 {/if}
               </div>
@@ -348,7 +396,7 @@
       {/snippet}
     </Popover.Trigger>
     <Popover.Portal>
-      <Popover.Content forceMount side="bottom" sideOffset={8} align="center" collisionPadding={8} class={cn("z-30 min-w-[32rem] rounded-lg px-8 py-4", $performanceMode ? "bg-background-grey/95" : "bg-background-grey/30 backdrop-blur-lg backdrop-brightness-50")}>
+      <Popover.Content forceMount side="bottom" sideOffset={8} align="center" collisionPadding={8} class={cn("z-30 w-[32rem] rounded-lg px-8 py-4", $performanceMode ? "bg-background-grey/95" : "bg-background-grey/30 backdrop-blur-lg backdrop-brightness-50")}>
         {#snippet child({ wrapperProps, props, open })}
           {#if open}
             <div {...wrapperProps}>
