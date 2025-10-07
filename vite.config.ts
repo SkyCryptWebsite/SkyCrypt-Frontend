fb9c45d70a366c5ae5cbfd1fed3a7ab09d9d8ebd
DarthGigi <47110839+DarthGigi@users.noreply.github.com>
2025-10-07 04:44:57 +0200

    feat: setup tests with vitest and webdriverio

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { sentrySvelteKit } from "@sentry/sveltekit";
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
 import devtoolsJson from "vite-plugin-devtools-json";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [
@@ -16,7 +16,35 @@ export default defineConfig({
     sveltekit(),
     devtoolsJson()
   ],
-  build: {
-    sourcemap: true
+  build: { sourcemap: true },
+  test: {
+    expect: { requireAssertions: true },
+    projects: [
+      {
+        extends: "./vite.config.ts",
+        test: {
+          name: "client",
+          environment: "browser",
+          browser: {
+            enabled: true,
+            provider: "webdriverio",
+            instances: [{ browser: "chrome" }],
+            headless: true
+          },
+          include: ["src/**/*.svelte.{test,spec}.{js,ts}"],
+          exclude: ["src/lib/server/**"],
+          setupFiles: ["./vitest-setup-client.ts"]
+        }
+      },
+      {
+        extends: "./vite.config.ts",
+        test: {
+          name: "server",
+          environment: "node",
+          include: ["src/**/*.{test,spec}.{js,ts}"],
+          exclude: ["src/**/*.svelte.{test,spec}.{js,ts}"]
+        }
+      }
+    ]
   }
 });
