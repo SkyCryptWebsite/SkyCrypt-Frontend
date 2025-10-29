ea50573e902cdd8371b7976cdef0c93272fcc109
DarthGigi <47110839+DarthGigi@users.noreply.github.com>
2025-10-30 00:53:57 +0100

    fix: handle non-GET requests and chrome-extension URLs in fetch event

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -46,11 +46,11 @@ self.addEventListener("activate", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
-  // ignore POST requests etc
-  if (event.request.method !== "GET") return;
+  const url = new URL(event.request.url);
+  // ignore POST requests, chrome-extension requests, etc
+  if (event.request.method !== "GET" || url.protocol === "chrome-extension:") return;
 
   async function respond() {
-    const url = new URL(event.request.url);
     const cache = await caches.open(CACHE);
 
     // `build`/`files` can always be served from the cache
