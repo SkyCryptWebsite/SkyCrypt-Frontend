813b34a87f00109e754901e51fbdec9cb500dfa5
Connor Lewis <50084106+imconnorngl@users.noreply.github.com>
2026-01-27 19:55:12 +0000

    chore: deploy to cf workers

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-node";
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /**
@@ -35,9 +35,6 @@ const config = {
         server: true
       }
     },
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
     alias: {
       $params: "./src/params",
