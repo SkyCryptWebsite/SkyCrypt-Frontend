65b3371358ae657424051c2ff5c2f9ea0718feab
DarthGigi <47110839+DarthGigi@users.noreply.github.com>
2026-06-17 01:01:13 +0200

    chore: prettier format

--- src/lib/components/ui/avatar/avatar-image.svelte
+++ src/lib/components/ui/avatar/avatar-image.svelte
@@ -1,17 +1,8 @@
 <script lang="ts">
-	import { Avatar as AvatarPrimitive } from "bits-ui";
-	import { cn } from "$utils.js";
+  import { Avatar as AvatarPrimitive } from "bits-ui";
+  import { cn } from "$utils.js";
 
-	let {
-		ref = $bindable(null),
-		class: className,
-		...restProps
-	}: AvatarPrimitive.ImageProps = $props();
+  let { ref = $bindable(null), class: className, ...restProps }: AvatarPrimitive.ImageProps = $props();
 </script>
 
-<AvatarPrimitive.Image
-	bind:ref
-	data-slot="avatar-image"
-	class={cn("rounded-full aspect-square size-full object-cover", className)}
-	{...restProps}
-/>
+<AvatarPrimitive.Image bind:ref data-slot="avatar-image" class={cn("rounded-full aspect-square size-full object-cover", className)} {...restProps} />
