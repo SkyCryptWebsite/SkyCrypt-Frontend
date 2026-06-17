3c2aa6c48b59d4e233b38021757c07b091d09014
DarthGigi <47110839+DarthGigi@users.noreply.github.com>
2026-06-18 01:48:05 +0200

    chore: update shadcn-svelte components

--- src/lib/shared/utils.ts
+++ src/lib/shared/utils.ts
@@ -3,11 +3,7 @@ import { cubicOut } from "svelte/easing";
 import type { TransitionConfig } from "svelte/transition";
 import { twMerge } from "tailwind-merge";
 
-/**
- * @param {ClassValue[]} inputs The classes to merge
- * @returns {string} The merged classes
- */
-export function cn(...inputs: ClassValue[]): string {
+export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
